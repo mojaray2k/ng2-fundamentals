28231cf7b9f3234b327874f696fbc2e72b7ccf7d
Amen Moja Ra <amenra@Amens-Mac-mini.local>
2017-03-16 03:09:32 -0400

    update to include content projection

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,9 @@
-
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveErrors } from '@angular/forms/src/directives/reactive_errors';
+
 import {
     EventsListComponent,
     EventThumbnailComponent,
@@ -9,18 +11,23 @@ import {
     EventDetailsComponent,
     CreateEventComponent,
     EventRouteActivator,
-    EventListResolver
-
+    EventListResolver,
+    CreateSessionComponent,
+    SessionListComponent
 } from './events/index'
+
 import { EventsAppComponent } from './events.app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { ToastrService } from './common/toastr.service';
+import { CollapsibleWellComponent } from './common/collapsible-well.component';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
     declarations: [
@@ -30,7 +37,10 @@ import { AuthService } from './user/auth.service';
         EventThumbnailComponent,
         EventDetailsComponent,
         CreateEventComponent,
-        Error404Component
+        Error404Component,
+        CreateSessionComponent,
+        SessionListComponent,
+        CollapsibleWellComponent
     ],
     providers: [
         EventService,
@@ -50,6 +60,6 @@ export class AppModule {}
 
 function checkDirtyState(component:CreateEventComponent){
     if(component.isDirty)
-      return window.confirm("You have not saved this event, doyou really wantto cancel?")
+      return window.confirm("You have not saved this event, do you really want to cancel?")
     return true
 }
--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,25 +1,34 @@
+import { EventService } from './shared/event.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    template: 
-    `
-        <h1>New Event</h1>
-        <hr>
-        <div class="col-md-6">
-            <h3>[Create Event Form will go here]</h3>
-            <br>
-            <br>
-            <button type="submit" class="btn btn-primary">Save</button>
-            <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
-        </div>
-    `
+    templateUrl: 'app/events/create-event.component.html',
+    styles: [
+      `
+      em{float: right; color: #E85C65; padding-left:10px;}
+      .error input{background-color:#E3C3C5;}
+      .error ::-webkit-input-placeholder {color: #999;}
+      .error ::-moz-placeholder {color: #999;}
+      .error :-moz-placeholder {color: #999;}
+      .error :ms-input-placeholder {color: #999;}
+      `
+    ]
 })
 export class CreateEventComponent implements OnInit {
     isDirty:boolean = true
-    constructor(private router: Router) { }
 
-    ngOnInit() { }
+    constructor(private router: Router, private eventService:EventService) { }
+
+    ngOnInit() {
+        
+     }
+    
+    saveEvent(formValues){
+        this.eventService.saveEvent(formValues)
+        this.isDirty = false
+        this.router.navigate(['/events'])
+    }
 
     cancel(){
         this.router.navigate(['/events'])
