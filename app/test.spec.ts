fddb4c05e280835d1b13d678daea40226d46c1fb
Amen Moja Ra <amenra@Amens-Mac-mini.local>
2017-03-20 19:28:53 -0400

    Added test template to the repo

--- app/test.spec.ts
+++ app/test.spec.ts
@@ -1,4 +1,5 @@
-describe('first tests', () => {
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+describe('first test', () => {
     it('should be true', () => {
         expect(true).toBe(true);
     })
